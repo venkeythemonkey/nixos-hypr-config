9f33d334ed35fd06351b51d761dab9456019516a
venkeythemonkey <venkie426@gmail.com>
2026-08-08 00:29:39 +0530

    feat: add Noctalia clipboard shortcut

--- dotfiles/hypr/.config/hypr/config/programs.lua
+++ dotfiles/hypr/.config/hypr/config/programs.lua
@@ -2,6 +2,7 @@ return {
     terminal           = "kitty",
     fileManager        = "dolphin",
     appLauncher        = "noctalia msg panel-toggle launcher",
+    clipboard          = "noctalia msg panel-toggle clipboard",
     windowSwitcher     = "noctalia msg window-switcher",
     sessionMenu        = "noctalia msg panel-toggle session",
     screenshotRegion   = "noctalia msg screenshot-region",
