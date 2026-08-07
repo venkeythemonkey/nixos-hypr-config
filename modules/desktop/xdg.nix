8dc9782e108a1140077b5e008cb0b9805fcbecc9
venkeythemonkey <venkie426@gmail.com>
2026-08-08 01:44:44 +0530

    fix: set image MIME defaults for Dolphin

--- modules/desktop/xdg.nix
+++ modules/desktop/xdg.nix
@@ -1,5 +1,23 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
+let
+  imageMimeTypes = [
+    "image/gif"
+    "image/jpeg"
+    "image/png"
+    "image/webp"
+  ];
+
+  setImageMimeDefaults = pkgs.writeShellScript "set-image-mime-defaults" ''
+    set -eu
+
+    for mimeType in ${lib.escapeShellArgs imageMimeTypes}; do
+      ${pkgs.xdg-utils}/bin/xdg-mime default org.kde.gwenview.desktop "$mimeType"
+    done
+
+    ${pkgs.kdePackages.kservice}/bin/kbuildsycoca6 --noincremental
+  '';
+in
 {
   xdg.mime.defaultApplications = {
     "application/pdf" = "org.kde.okular.desktop";
@@ -22,6 +40,17 @@
       Type = "oneshot";
       ExecStart = "${pkgs.xdg-user-dirs}/bin/xdg-user-dirs-update";
       ExecStartPost = "${pkgs.coreutils}/bin/mkdir -p %h/Pictures/Screenshots";
+    };
+  };
+
+  # Keep KDE's user-level application database aligned with the system defaults.
+  systemd.user.services.xdg-image-mime-defaults = {
+    description = "Set image MIME defaults for KDE applications";
+    wantedBy = [ "default.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = setImageMimeDefaults;
     };
   };
 }
