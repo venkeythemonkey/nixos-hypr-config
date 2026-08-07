26d99c31620b7640eadbec1008d0998e8f333f2b
venkeythemonkey <venkie426@gmail.com>
2026-08-08 00:28:13 +0530

    feat: open Noctalia session menu from Hyprland

--- dotfiles/hypr/.config/hypr/config/programs.lua
+++ dotfiles/hypr/.config/hypr/config/programs.lua
@@ -3,6 +3,7 @@ return {
     fileManager        = "dolphin",
     appLauncher        = "noctalia msg panel-toggle launcher",
     windowSwitcher     = "noctalia msg window-switcher",
+    sessionMenu        = "noctalia msg panel-toggle session",
     screenshot         = "flameshot gui",
     noctaliaBarRefresh = "noctalia msg bar-hide && noctalia msg bar-show",
 }
